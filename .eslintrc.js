6073d4b87c0a183c47df2223ff8038729d619f57
anson-li <ansonli.94@gmail.com>
2021-11-21 16:49:03 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+  // parser: {
+  //   'babel-eslint'
+  // }
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-plusplus */
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ /g);
+  const command = args.shift().toLowerCase();
+
+  // FKU FUNCTION
+  if (command === 'fku') {
+    message.channel.send('fk you too');
+  }
+
+  // HEADS OR TAILS FUNCTION
+  if (command === 'hort') {
+    const rand = Math.random() < 0.5;
+
+    if (rand) {
+      message.channel.send('Heads', { files: ['https://i.imgur.com/BguRU0K.png'] });
+    } else {
+      message.channel.send('Tails', { files: ['https://i.imgur.com/u0lKXpp.png'] });
+    }
+  }
+
+  // TEST FUNCTION
+  if (command === 'test') {
+    if (!args.length) {
+      message.channel.send(`testing usertag: ${message.author}!`);
+      return;
+    }
+    message.channel.send(`arguments: ${args}`);
+  }
+});
+
+client.login(process.env.CLIENT_TOKEN);
